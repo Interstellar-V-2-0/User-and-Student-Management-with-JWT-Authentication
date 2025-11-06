e31aac5106e9929b91d98b38d34457f04dcf19ae
Daniels0809 <do7269301@gmail.com>
2025-11-06 15:28:06 -0500

    Agrega UserStudentMgmtDbContext.cs al repositorio remoto

--- UserStudentMgmt.Infrastructure/Data/UserStudentMgmtDbContext.cs
+++ UserStudentMgmt.Infrastructure/Data/UserStudentMgmtDbContext.cs
@@ -1,6 +1,71 @@
-namespace UserStudentMgmt.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using UserStudentMgmt.Domain.Entities;
 
-public class UserStudentMgmtDbContext
+namespace UserStudentMgmt.Infrastructure.Data
 {
-    
+    public class UserStudentMgmtDbContext : DbContext
+    {
+        public UserStudentMgmtDbContext(DbContextOptions<UserStudentMgmtDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<DocumentType> DocumentTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<DocumentType>(entity =>
+            {
+                entity.ToTable("DocumentTypes");
+
+                entity.HasKey(dt => dt.Id);
+
+                entity.Property(dt => dt.Name)
+                      .IsRequired()
+                      .HasMaxLength(50);
+
+                entity.HasMany(dt => dt.Users)
+                      .WithOne(u => u.DocType)
+                      .HasForeignKey(u => u.DocTypeId)
+                      .OnDelete(DeleteBehavior.Restrict);
+            });
+            
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("Users");
+
+                entity.HasKey(u => u.Id);
+
+                entity.Property(u => u.Name)
+                      .IsRequired()
+                      .HasMaxLength(100);
+
+                entity.Property(u => u.LastName)
+                      .IsRequired()
+                      .HasMaxLength(100);
+
+                entity.Property(u => u.DocumentNumber)
+                      .IsRequired()
+                      .HasMaxLength(20);
+
+                entity.Property(u => u.Email)
+                      .IsRequired()
+                      .HasMaxLength(150);
+
+                entity.Property(u => u.PhoneNumber)
+                      .HasMaxLength(20);
+
+                entity.Property(u => u.UserName)
+                      .IsRequired()
+                      .HasMaxLength(50);
+
+                entity.Property(u => u.PasswordHash)
+                      .IsRequired()
+                      .HasMaxLength(255);
+            });
+        }
+    }
 }
